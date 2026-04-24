4e096ac513013293cc3da325c25c2f806ad7706d
Mohd Adil <shaikhmdadil191@gmail.com>
2026-04-24 22:48:56 +0530

    feat: implement grocery planner and insights screens with supporting components

--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import ClearCompletedButton from "@/components/insights/ClearCompletedButton";
+import InsightsCategorySection from "@/components/insights/InsightsCategorySection";
+import InsightsPrioritySection from "@/components/insights/InsightsPrioritySection";
+import InsightsStatsSection from "@/components/insights/InsightsStatsSection";
+import UserProfile from "@/components/insights/UserProfile";
+import TabScreenBackground from "@/components/TabScreenBackground";
+import { ScrollView } from "react-native";
 
-const InsightsScreen  = () => {
+const InsightsScreen = () => {
   return (
-    <View>
-      <Text>insights</Text>
-    </View>
-  )
-}
+    <>
+      <ScrollView
+        className="flex-1 bg-background py-4"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ padding: 20, gap: 14 }}
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        <TabScreenBackground />
 
-export default InsightsScreen 
+        <UserProfile />
+        <InsightsStatsSection />
+        <InsightsCategorySection />
+        <InsightsPrioritySection />
+        <ClearCompletedButton />
+      </ScrollView>
+
+    </>
+  );
+};
+
+export default InsightsScreen;
